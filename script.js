de0127c10b3f376e34f2f70a539d60eebe7faa28
AABID DEVELOPER <150765769+Aabid404@users.noreply.github.com>
2025-07-30 14:12:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 const makeChange = (c) => {
   // your name here
+	let change = {};
+
+  change.q = Math.floor(cents / 25);
+  cents %= 25;
+
+  change.d = Math.floor(cents / 10);
+  cents %= 10;
+
+  change.n = Math.floor(cents / 5);
+  cents %= 5;
+
+  change.p = cents;
+
+  return change;
 };
 
 // Do not the change the code below
